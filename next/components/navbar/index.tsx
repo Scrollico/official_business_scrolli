aa0c72bc94981fac177ae4c6fabfc8a65ac59fba
nihat <nihtavci@gmail.com>
2025-10-04 17:38:05 +0200

    Fix hydration mismatch: revert z-index to z-50

--- next/components/navbar/index.tsx
+++ next/components/navbar/index.tsx
@@ -7,7 +7,7 @@ import { MobileNavbar } from './mobile-navbar';
 
 export function Navbar({ data, locale }: { data: any; locale: string }) {
   return (
-    <motion.nav className="max-w-7xl  fixed top-4  mx-auto inset-x-0 z-[9999] w-[95%] lg:w-full">
+    <motion.nav className="max-w-7xl  fixed top-4  mx-auto inset-x-0 z-50 w-[95%] lg:w-full">
       <div className="hidden lg:block w-full">
         {data?.left_navbar_items && (
           <DesktopNavbar
